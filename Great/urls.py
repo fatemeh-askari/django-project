da5be4b0651508cb200b7fc094c1e8308ececd7b
fatemeh <fatemeh.askari.edu@gmail.com>
2023-09-24 10:35:06 +0330

    before product variation preparation

--- Great/urls.py
+++ Great/urls.py
@@ -23,7 +23,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
-    path('store/', include('store.urls'))
+    path('store/', include('store.urls')),
+    path('cart/', include('carts.urls')),
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
