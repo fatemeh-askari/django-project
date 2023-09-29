6bff5a309060a12788e61272617e8e4d298761fc
fatemeh <fatemeh.askari.edu@gmail.com>
2023-09-29 14:14:30 +0330

    Search Box

--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from carts.models import CartItem
@@ -48,3 +49,17 @@ def product_detail(request, category_slug, product_slug):
     }
 
     return render(request, 'store/product_detail.html', context)
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+        context = {
+            'products': products,
+            'product_count': product_count,
+        }
+
+    return render(request, 'store/store.html', context)
