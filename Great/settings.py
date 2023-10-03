a1f00b9c2edd430ddd1315a5de349c438a4178ba
fatemeh <fatemeh.askari.edu@gmail.com>
2023-10-03 22:34:57 +0330

    Email Activation Part 1

--- Great/settings.py
+++ Great/settings.py
@@ -145,3 +145,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.INFO: "danger",
 }
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'info.rappidweb@gmail.com'
+EMAIL_HOST_PASSWORD = 'cpxagpoizpixeqbs'
+EMAIL_USE_TLS = True
