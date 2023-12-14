0b794dae5c098676b145666a312c26b18bb88b4b
fatemeh <fatemeh.askari.edu@gmail.com>
2023-12-14 11:18:21 +0330

    Cart Counter

--- carts/views.py
+++ carts/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from carts.models import Cart, CartItem
 from store.models import Product, Variation
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -112,6 +113,34 @@ def cart(request, total=0, quantity=0, cart_items=None):
     try:
         tax = 0
         grand_total = 0
+        if request.user.is_authenticated:
+            cart_items = CartItem.objects.filter(user=request.user, is_active=True)
+        else:
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+            cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        tax = (2 * total) / 100
+        grand_total = total + tax
+    except ObjectDoesNotExist:
+        pass  # just ignore
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        'tax': tax,
+        'garnd_total': grand_total,
+    }
+    return render(request, 'store/cart.html', context)
+
+
+@login_required(login_url='login')
+def checkout(request, total=0, quantity=0, cart_items=None):
+    try:
+        tax = 0
+        grand_total = 0
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
@@ -128,4 +157,4 @@ def cart(request, total=0, quantity=0, cart_items=None):
         'tax': tax,
         'garnd_total': grand_total,
     }
-    return render(request, 'store/cart.html', context)
+    return render(request, 'store/checkout.html', context)
