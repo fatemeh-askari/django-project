28456de45c5d36daab5b40274e50c6557717953e
fatemeh <fatemeh.askari.edu@gmail.com>
2024-06-03 01:33:17 +0430

    Place Order View and Generate Order Number

--- Great/urls.py
+++ Great/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('store/', include('store.urls')),
     path('cart/', include('carts.urls')),
     path('accounts/', include('accounts.urls')),
+    path('orders/', include('orders.urls')),
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,70 @@
-from django.shortcuts import render
+from datetime import datetime
+
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from carts.models import CartItem
+from .forms import OrderForm
+from .models import Order
+
 
 # Create your views here.
+def place_order(request, total=0, quantity=0,):
+    current_user = request.user
+
+    cart_items = CartItem.objects.filter(user=current_user)
+    cart_count = cart_items.count()
+
+    if cart_count <= 0:
+        return redirect('store')
+
+    grand_total = 0
+    tax = 0
+
+    for cart_item in cart_items:
+        total += (cart_item.product.price * cart_item.quantity)
+        quantity += cart_item.quantity
+    tax = (2 * total) / 100
+    grand_total = total + tax
+
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            # store all information
+            data = Order()
+            data.user = current_user
+            data.first_name = form.cleaned_data['first_name']
+            data.last_name = form.cleaned_data['last_name']
+            data.address_line_1 = form.cleaned_data['address_line_1']
+            data.address_line_2 = form.cleaned_data['address_line_2']
+            data.country = form.cleaned_data['country']
+            data.state = form.cleaned_data['state']
+            data.city = form.cleaned_data['city']
+            data.phone = form.cleaned_data['phone']
+            data.email = form.cleaned_data['email']
+            data.order_note = form.cleaned_data['order_note']
+            data.order_total = grand_total
+            data.tax = tax
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            ## generate order number
+            # yr = int(datetime.date.today().strftime('%Y'))
+            # dt = int(datetime.date.today().strftime('%d'))
+            # mt = int(datetime.date.today().strftime('%m'))
+            # d = datetime.date(yr, mt, dt)
+            # current_date = d.strftime('%Y%m%d')
+            # order_number = current_date + str(data.id)
+            current_date = datetime.today().strftime('%Y%m%d')
+            order_number = current_date + str(data.id)
+            data.order_number = order_number
+            data.save()
+            return redirect('checkout')
+    else:
+        return redirect('checkout')
+
+
+
+
+
+
+    # return HttpResponse('ok')
